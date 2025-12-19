900a142e320628e3d685a969111863151e783b1d
SpawniX <spawnieX@proton.me>
2025-12-19 11:33:21 -0300

    Update Main.js

--- js/Main.js
+++ js/Main.js
@@ -1,1 +1,15 @@
+<script>
+  document.querySelectorAll("gy").forEach(gy => {
+    const imgSrc = gy.getAttribute("img");
+    const text = gy.innerHTML;
 
+    gy.innerHTML = `
+      <div class="gy-img">
+        <img src="${imgSrc}" alt="gallery image">
+      </div>
+      <div class="gy-text">
+        ${text}
+      </div>
+    `;
+  });
+</script>
